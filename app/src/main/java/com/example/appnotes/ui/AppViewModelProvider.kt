5b594036965cb8c70cc536cc16dd8a1c4794cbdc
nayeli <nayeliserritos@gmail.com>
2025-10-25 17:12:05 -0600

    Se creó el viewModel base para la pantalla principal y se usó el patrón de factory para crear y llamar al viewModel cada vez que haya un cambio en los datos o cambios de configuración, manteniendo correctamente el ciclo de vida de la app.

--- app/src/main/java/com/example/appnotes/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/appnotes/ui/AppViewModelProvider.kt
@@ -1,2 +1,17 @@
 package com.example.appnotes.ui
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.appnotes.NotesApplication
+import com.example.appnotes.ui.home.HomeViewModel
+
+object HomeViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            val application = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as NotesApplication
+            val repository = application.container.notesRepository
+            HomeViewModel(repository)
+        }
+    }
+}
--- app/src/main/java/com/example/appnotes/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/appnotes/ui/home/HomeScreen.kt
@@ -1,2 +1,15 @@
 package com.example.appnotes.ui.home
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.appnotes.ui.HomeViewModelProvider
+
+@Composable
+fun HomeScreen(
+    viewModel: HomeViewModel = viewModel(factory = HomeViewModelProvider.Factory)
+) {
+
+}
